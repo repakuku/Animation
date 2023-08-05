f588480b3c40fcab88d6a78943b36dfefbc9f53d
Alexey Turulin <repakuku@icloud.com>
2023-08-05 11:48:07 -0400

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Alexey Turulin on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
